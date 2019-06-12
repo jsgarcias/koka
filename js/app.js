5c5b0946d53389915973ebac4236f41b2d3ea9a9
juanpaguti <jpgutierrez@digitalrising.com.gt>
2019-06-12 15:51:03 -0600

    revision majo gutierrez

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 2000);
+}
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDivLanding").style.display = "block";
+}
